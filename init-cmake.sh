187c64b9a274f671d48cb7a2b860f70db566a514
Ben Dod <benjdod@live.unc.edu>
2022-05-26 19:21:02 -0400

    messages in init-cmake, no proj defn in subdirs

--- init-cmake.sh
+++ init-cmake.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 mkdir -p build
+echo "removing previous build files"
 rm -r build/*
+echo "creating build targets"
 mkdir -p build/release build/debug
 
 cmake -S . -B build/debug 	-DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_EXPORT_COMPILE_COMMANDS=1
